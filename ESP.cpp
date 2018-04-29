e5b571560e507b98f559af8344036d0be922cab0
faggotman69 <38589819+faggotman69@users.noreply.github.com>
2018-04-28 22:09:34 -0500

    worldtoscreen and player check to offscreen indicator

--- ESP.cpp
+++ ESP.cpp
@@ -251,10 +251,11 @@ void visuals::OnPaintTraverse(C_BaseEntity* local)
 
 			if (pLocal->GetTeamNum() != entity->GetTeamNum())
 			{
-//				if you wanted a w2c check, do this:
+				if (entity->GetClientClass()->m_ClassID == (int)ClassID::CCSPlayer) //hostages show up for some reason
+					continue;
 				
-//				if (g_Render->WorldToScreen(pos3D, pos))
-//					continue;
+				if (g_Render->WorldToScreen(pos3D, pos)) //checks WorldToScreen and if so, continue
+					continue;
 				
 				Vector eyeangles, poopvec;
 				int screen_w, screen_h;
